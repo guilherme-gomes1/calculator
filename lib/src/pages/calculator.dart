df43c78ee5352d3e64520a95867abf27180dc536
Guilherme Gomes <guilhermegg018@gmail.com>
2023-03-07 01:38:34 -0300

    interface e funcionalidades prontas

--- lib/src/pages/calculator.dart
+++ lib/src/pages/calculator.dart
@@ -2,10 +2,10 @@ import 'package:calculator/src/components/display.dart';
 import 'package:calculator/src/components/keyboard.dart';
 import 'package:calculator/src/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
-
-  const Calculator({ Key? key }) : super(key: key);
+  const Calculator({Key? key}) : super(key: key);
 
   @override
   State<Calculator> createState() => _CalculatorState();
@@ -14,21 +14,26 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   final Memory memory = Memory();
 
-  _onPressed(String command){
+  _onPressed(String command) {
     setState(() {
       memory.applyCommand(command);
     });
   }
 
-   @override
-   Widget build(BuildContext context) {
-       return MaterialApp(
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return MaterialApp(
         home: Column(
-          children: [
-            Display(text: memory.value),
-            Keyboard(onPressed: _onPressed,)
-          ],
+      children: [
+        Display(text: memory.value),
+        Keyboard(
+          onPressed: _onPressed,
         )
-       );
+      ],
+    ));
   }
 }
